78e5bdd37893403f727f1527e550b9a854734fe7
mlizz <elizabeth.reed.3@ndsu.edu>
2026-03-18 14:16:50 -0500

    Added routing to the angular application

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from '@angular/core';
+import { NgFor } from '@angular/common';
 import { HousingLocation } from '../housing-location/housing-location';
 import { HousingLocationInfo } from '../housinglocationinfo';
 import {HousingService} from '../housing-service';
@@ -6,17 +7,30 @@ import {HousingService} from '../housing-service';
 
 @Component({
   selector: 'app-home',
-  imports: [HousingLocation],
+  imports: [NgFor, HousingLocation],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
   // readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
   housingLocationList: HousingLocationInfo[] = [];
+  filterString = '';
+
   // Injecting HousingService into the Component
   housingService: HousingService = inject(HousingService);
+
   constructor(){
-  //setting housingLocationlist to the data array in HousingService
-  this.housingLocationList = this.housingService.getAllHousingLocations();
+    // setting housingLocationlist to the data array in HousingService
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
+
+  get filteredHousingLocationList(): HousingLocationInfo[] {
+    const filter = this.filterString?.trim().toLowerCase();
+    if (!filter) {
+      return this.housingLocationList;
+    }
+    return this.housingLocationList.filter((location) =>
+      location.city.toLowerCase().includes(filter)
+    );
   }
 }
